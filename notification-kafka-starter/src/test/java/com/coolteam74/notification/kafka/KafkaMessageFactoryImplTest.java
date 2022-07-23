d900947c97835be3b5ff3c8074f8a73b585635ad
Andrei Poliakov <avpgenium@yandex.ru>
2022-07-23 16:58:36 +0300

    add embedded kafka for tests

--- notification-kafka-starter/src/test/java/com/coolteam74/notification/kafka/KafkaMessageFactoryImplTest.java
+++ notification-kafka-starter/src/test/java/com/coolteam74/notification/kafka/KafkaMessageFactoryImplTest.java
@@ -5,10 +5,14 @@ import com.coolteam74.mq.notification.core.model.NotificationMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTest(properties = {"mq.notification=kafka"})
+@DirtiesContext
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 class KafkaMessageFactoryImplTest {
 
     @Autowired
